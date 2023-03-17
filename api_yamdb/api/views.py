dfee6c502bf3913627659320b3ac67552c9eeefa
Лев Андреев <leoohard@yandex.ru>
2023-03-17 18:26:40 +0300

    правки оценки рейтинга

--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -26,7 +26,7 @@ class ReviewViewSet(viewsets.ModelViewSet):
         scores = reviews.values_list('score', flat=True)
         scores = list(scores)
         scores.append(serializer.validated_data['score'])
-        if scores == 0:
+        if scores is None:
             return 'Оценок ещё нет'
         rating = sum(scores) / len(scores)
         title.rating = round(rating)
