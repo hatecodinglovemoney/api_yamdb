61356b0a8aadee826c60d40268b194e982b15a03
Лев Андреев <leoohard@yandex.ru>
2023-03-17 20:08:58 +0300

    правки urls

--- api_yamdb/api/urls.py
+++ api_yamdb/api/urls.py
@@ -14,8 +14,14 @@ v1_router = routers.DefaultRouter()
 v1_router.register('titles', TitleViewSet, basename='title')
 v1_router.register('genres', GenreViewSet, basename='genre')
 v1_router.register('categories', CategoryViewSet, basename='categories')
-v1_router.register('review', ReviewViewSet, basename='review')
-v1_router.register('comment', CommentViewSet, basename='comment')
+v1_router.register(
+    r'titles/(?P<title_id>\d+)/review',
+    ReviewViewSet, basename='review'
+)
+v1_router.register(
+    r'titles/(?P<title_id>\d+)/review/(?P<review_id>\d+)/comments',
+    CommentViewSet, basename='comment'
+)
 
 urlpatterns = [
     path('v1/', include(v1_router.urls)),
