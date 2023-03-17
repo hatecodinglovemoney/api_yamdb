7ba23194ef2da02bef8b9655c7f75b1ea118631a
Kirill Sobolev <sblvkr@gmail.com>
2023-03-17 17:31:46 +0300

    Импорт csv файлов

---
+++ api_yamdb/reviews/management/commands/_orm_func_for_import.py
@@ -0,0 +1,68 @@
+from reviews.models import (Category, Comments, Genre, GenreTitle, Reviews,
+                            Title, User)
+
+
+def category_create(row):
+    Category.objects.create(
+        id=row[0],
+        name=row[1],
+        slug=row[2],
+    )
+
+
+def genre_create(row):
+    Genre.objects.create(
+        id=row[0],
+        name=row[1],
+        slug=row[2],
+    )
+
+
+def title_create(row):
+    Title.objects.create(
+        id=row[0],
+        name=row[1],
+        year=row[2],
+        category_id=row[3],
+    )
+
+
+def genre_title_create(row):
+    GenreTitle.objects.create(
+        id=row[0],
+        title_id=row[1],
+        genre_id=row[2],
+    )
+
+
+def user_create(row):
+    User.objects.create(
+        id=row[0],
+        username=row[1],
+        email=row[2],
+        role=row[3],
+        bio=row[4],
+        first_name=row[5],
+        last_name=row[6],
+    )
+
+
+def review_create(row):
+    Reviews.objects.create(
+        id=row[0],
+        title_id=row[1],
+        text=row[2],
+        author_id=row[3],
+        score=row[4],
+        pub_date=row[5],
+    )
+
+
+def comment_create(row):
+    Comments.objects.create(
+        id=row[0],
+        reviews_id=row[1],
+        text=row[2],
+        author_id=row[3],
+        pub_date=row[4],
+    )
