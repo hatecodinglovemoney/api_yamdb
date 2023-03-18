439517a1559ffdf921d4c64c97f6226aa0156e43
Лев Андреев <leoohard@yandex.ru>
2023-03-18 14:31:46 +0300

    добавление подсчёта среднего рейтинга и связи отывов с произведениями и комментариев с отзывами

--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -1,6 +1,8 @@
+from django.shortcuts import get_object_or_404
 from rest_framework import mixins, viewsets, filters
 from rest_framework.pagination import PageNumberPagination
 from django_filters.rest_framework import DjangoFilterBackend
+from django.db.models import Avg
 
 
 from api.serializers import (CategorySerializer,
@@ -9,21 +11,38 @@ from api.serializers import (CategorySerializer,
                              ReviewSerializer,
                              TitleGetSerializer,
                              TitlePostSerializer)
-from reviews.models import Category, Comment, Genre, Review, Title
+from reviews.models import Category, Genre, Review, Title
 
 
 class ReviewViewSet(viewsets.ModelViewSet):
     queryset = Review.objects.all()
     serializer_class = ReviewSerializer
 
+    def get_queryset(self):
+        title = get_object_or_404(Title, pk=self.kwargs.get('title_id'))
+        return title.review.select_related('title', 'author')
+
+    def perform_create(self, serializer):
+        title = get_object_or_404(Title, pk=self.kwargs.get('title_id'))
+        serializer.save(author=self.request.user, title=title)
+
 
 class CommentViewSet(viewsets.ModelViewSet):
-    queryset = Comment.objects.all()
     serializer_class = CommentSerializer
+
+    def get_queryset(self):
+        review = get_object_or_404(Review, pk=self.kwargs.get('review_id'))
+        return review.comment.select_related('review', 'author')
+
+    def perform_create(self, serializer):
+        review = get_object_or_404(Review, pk=self.kwargs.get('review_id'))
+        serializer.save(author=self.request.user, review=review)
 
 
 class TitleViewSet(viewsets.ModelViewSet):
-    queryset = Title.objects.all()
+    queryset = Title.objects.all().aggregate(
+        rating=Avg('rating__review__score')
+    )
     filter_backends = (DjangoFilterBackend,)
     filterset_fields = ('name', 'year', 'genre__slug', 'category__slug')
     pagination_class = PageNumberPagination
