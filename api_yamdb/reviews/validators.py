b41f4ed4c9948a1c1a731a6d8245a3130b3c0f89
Kirill Sobolev <sblvkr@gmail.com>
2023-03-23 14:04:43 +0300

    Исправил ошибку валидатора года произведений

--- api_yamdb/reviews/validators.py
+++ api_yamdb/reviews/validators.py
@@ -4,8 +4,8 @@ import datetime as dt
 from rest_framework import serializers
 from django.core.exceptions import ValidationError
 
-ERROR_YEAR_FROM_FUTURE = ('Вы ввели {entered_year} год, но год выпуска '
-                          'не может быть больше текущего ({current_year})!')
+ERROR_YEAR_FROM_FUTURE = ('Год выпуска ({entered_year}), не может '
+                          'быть больше текущего ({current_year})!')
 LEGAL_CHARACTERS = re.compile(r'^[\w.@+-]+$')
 LEGAL_CHARACTERS_ERROR = ('Нельзя использовать символ(ы): '
                           '{forbidden_chars} в имени пользователя.')
